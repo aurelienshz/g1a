2d50591cef775d38cb907014d841ce4334ad3657
ohhopi <tristan.muratore@gmail.com>
2015-10-22 20:12:42 +0200

    Agencement contenu Blocs Suggestions

--- eventease/vues/accueil/index.php
+++ eventease/vues/accueil/index.php
@@ -21,36 +21,69 @@
         </form>
         <a href="#"><div id="boutoncreer">Créer un évènement</div></a>
     </section>
-    
-    <section id="suggestions">
-      <div class="row">
-        <div class="col">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
-        <div class="col">
-            <img src="<?php echo IMAGES . 'img.jpg' ?>" alt="Texte alternatif" />
-        </div>
-        <div class="col">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col">
-            <img src="<?php echo IMAGES . 'img.jpg' ?>" alt="Texte alternatif" />
-        </div>
-        <div class="col">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
-        <div class="col">
-            <img src="<?php echo IMAGES . 'img.jpg' ?>" alt="Texte alternatif" />   
-        </div>
-      </div>
-    </section>
-    
-    <section id="features">
-        <div class="col"><img src="<?php echo IMAGES . 'img.jpg' ?>" alt="Texte alternatif" /></div>
-        <div class="col"><img src="<?php echo IMAGES . 'img.jpg' ?>" alt="Texte alternatif" /></div>
-        <div class="col"><img src="<?php echo IMAGES . 'img.jpg' ?>" alt="Texte alternatif" /></div>
-    </section>
+    <section id="wrap_suggestions">
+		<div class="lin">
+			<p id="titles" >Rien que pour vous</p>
+		</div>
+		<div id="suggestions">
+		  <div class="row">
+			<div class="col"><p id="subtitles" >Une envie de ?</p></div>
+			<div class="col"><p id="subtitles" >Près de vous</p></div>
+			<div class="col"><p id="subtitles" >Tout de suite</p></div>
+		  </div>
+		  <div class="row">
+			<div class="col">			
+				<span class="triSelector"> < </span>
+				<div class="triContent">
+					<div class="firstArgument">Type</div>
+					<div class="otherArgument">Titre / Date & Heure / Lieu</div>
+					<div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse faucibus eros in ante egestas feugiat. Sed suscipit orci id nunc blandit, eget bibendum metus viverra. Nam et mattis risus. </div>
+				</div>
+				<span class="triSelector"> > </span>
+			</div>
+			<div class="col">			
+				<span class="triSelector"> < </span>
+				<div class="triContent">
+					<div class="firstArgument">Type</div>
+					<div class="otherArgument">Titre / Date & Heure / Lieu</div>
+					<div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse faucibus eros in ante egestas feugiat. Sed suscipit orci id nunc blandit, eget bibendum metus viverra. Nam et mattis risus. </div>
+				</div>
+				<span class="triSelector"> > </span>
+			</div>
+			<div class="col">			
+				<span class="triSelector"> < </span>
+				<div class="triContent">
+					<div class="firstArgument">Type</div>
+					<div class="otherArgument">Titre / Date & Heure / Lieu</div>
+					<div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse faucibus eros in ante egestas feugiat. Sed suscipit orci id nunc blandit, eget bibendum metus viverra. Nam et mattis risus. </div>
+				</div>
+				<span class="triSelector"> > </span>
+			</div>
+		  </div>
+		</div>
+	</section>
+	<section id="wrap_features">
+		<div class="lin"><p id="titles" >Comment ça marche ?</p></div>
+		<div id="features">
+			<div class="row">
+				<div class="col"><p id="subtitles" >Participer</p></div>
+				<div class="col"><p id="subtitles" >Partager</p></div>
+				<div class="col"><p id="subtitles" >Organiser</p></div>
+			</div>
+			<div class="col">			
+				<div class="triContent">
+				Un
+				</div>
+			</div>
+			<div class="col">			
+				<div class="triContent">
+				Deux
+				</div>
+			</div>
+			<div class="col">			
+				<div class="triContent">
+				Trois <!-- Soleil ! -->
+				</div>
+			</div>
+		</div>
+	</section>
