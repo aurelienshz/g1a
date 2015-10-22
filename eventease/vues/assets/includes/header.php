97cce488be8b238695ccc32d8c1a44559b0f4758
ohhopi <tristan.muratore@gmail.com>
2015-10-22 10:11:54 +0200

    Ajout d'icones sur le header
    Temporairement j’utilise le CDN de Bootstrap.

--- eventease/vues/assets/includes/header.php
+++ eventease/vues/assets/includes/header.php
@@ -10,6 +10,7 @@ A venir : distinction header connecté / non connecté (comment ? contrôleur ou
     <meta charset="utf-8" />
     <title><?php echo $title; ?> | EventEase</title>
     <link rel="stylesheet" href="<?php echo CSS.'style.css' ?>" type="text/css" media="all" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 </head>
 <body>
@@ -21,13 +22,13 @@ A venir : distinction header connecté / non connecté (comment ? contrôleur ou
         </h1>
         <nav>
           <ul id="raccourcis">
-            <li><a href="#">Accueil</a></li>
-            <li><a href="#">Créer</a></li>
-            <li><a href="#">Chercher</a></li>
+            <li><a href="#"><i class="fa fa-home fa-fw"></i>&nbsp;Accueil</a></li>
+            <li><a href="#"><i class="fa fa-plus"></i>&nbsp;Créer</a></li>
+            <li><a href="#"><i class="fa fa-search"></i>&nbsp;Chercher</a></li>
           </ul>
           <ul id="membre">
-            <li><a href="#">Connexion</a></li>
-            <li><a href="#">Inscription</a></li>
+            <li><a href="#"><i class="fa fa-sign-in"></i>&nbsp;Connexion</a></li>
+            <li><a href="#"><i class="fa fa-pencil-square-o"></i>&nbsp;Inscription</a></li>
           </ul>
         </nav>
     </header>
