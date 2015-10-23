f18861e5706b5db2911a8d0873af0e08e17d73a2
aurelien.schiltz <aurelien.schiltz@free.fr>
2015-10-23 13:51:11 +0200

    Met en forme features selon le nouveau pattern

--- eventease/vues/accueil/index.php
+++ eventease/vues/accueil/index.php
@@ -23,66 +23,58 @@
     </section>
     <section id="suggestions">
         <h4>Rien que pour vous</h4>
-		  <div class="row">
-			<div class="col"><h5>Une envie de ?</h5></div>
-			<div class="col"><h5>Près de vous</h5></div>
-			<div class="col"><h5 class="subtitles" >Tout de suite</h5></div>
-		  </div>
+		    <div class="row">
+                <div class="col"><h5>Une envie de ?</h5></div>
+                <div class="col"><h5>Près de vous</h5></div>
+                <div class="col"><h5 class="subtitles" >Tout de suite</h5></div>
+            </div>
 		  <div class="row">
 			<div class="col">			
-				<span class="triSelector"> &lt; </span>
+				<span class="triSelector">&lt;</span>
 				<div class="triContent">
 					<div class="firstArgument">Type</div>
-					<div class="otherArgument">Titre / Date & Heure / Lieu</div>
+					<div class="otherArgument">Titre | Date & Heure | Lieu</div>
 					<div class="description">Lorem ipsumtest dolor sit amet, consectetur adipiscing elit. Suspendisse faucibus eros in ante egestas feugiat. Sed suscipit orci id nunc blandit, eget bibendum metus viverra. Nam et mattis risus. </div>
 				</div>
-				<span class="triSelector"> &gt; </span>
+				<span class="triSelector">&gt;</span>
 			</div>
 			<div class="col">			
-				<span class="triSelector"> &lt; </span>
+				<span class="triSelector">&lt;</span>
 				<div class="triContent">
 					<div class="firstArgument">Type</div>
-					<div class="otherArgument">Titre / Date & Heure / Lieu</div>
+					<div class="otherArgument">Titre | Date & Heure | Lieu</div>
 					<div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse faucibus eros in ante egestas feugiat. Sed suscipit orci id nunc blandit, eget bibendum metus viverra. Nam et mattis risus. </div>
 				</div>
-				<span class="triSelector"> &gt; </span>
+				<span class="triSelector">&gt;</span>
 			</div>
 			<div class="col">			
-				<span class="triSelector"> &lt; </span>
+				<span class="triSelector">&lt;</span>
 				<div class="triContent">
 					<div class="firstArgument">Type</div>
-					<div class="otherArgument">Titre / Date & Heure / Lieu</div>
+					<div class="otherArgument">Titre | Date & Heure | Lieu</div>
 					<div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse faucibus eros in ante egestas feugiat. Sed suscipit orci id nunc blandit, eget bibendum metus viverra. Nam et mattis risus. </div>
 				</div>
-				<span class="triSelector"> &gt; </span>
+				<span class="triSelector">&gt;</span>
 			</div>
 		  </div>
 	</section>
-	<section id="wrap_features">
-		<div class="lin">
-            <p class="titles" >Comment ça marche ?</p>
-        </div>
-		<div id="features">
+	<section id="features">
+        <h4>Comment ça marche ?</h4>
 			<div class="row">
-				<div class="col"><p class="subtitles" >Participer</p></div>
-				<div class="col"><p class="subtitles" >Partager</p></div>
-				<div class="col"><p class="subtitles" >Organiser</p></div>
+				<div class="col"><h5>Participer </h5></div>
+				<div class="col"><h5>Partager   </h5></div>
+				<div class="col"><h5>Organiser  </h5></div>
 			</div>
-			<div class="col">			
-				<div class="triContent">
-				Un
-				</div>
-			</div>
-			<div class="col">			
-				<div class="triContent">
-				Deux
-				</div>
-			</div>
-			<div class="col">			
-				<div class="triContent">
-				Trois <!-- Soleil ! -->
-				</div>
-			</div>
-		</div>
+            <div class="row">
+                <div class="col">
+                    <p>Un</p>
+                </div>
+                <div class="col">
+                    Deux
+                </div>
+                <div class="col">
+                    Trois
+                </div>
+            </div>
 	</section>
 </div> <!-- /wrapper -->
