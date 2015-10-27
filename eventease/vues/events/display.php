866912957577e5d29abf4036d5b3cba3c4ff79c9
guillaume lachaud <guillaumelachaud@MacBook-Pro-de-guillaume.local>
2015-10-27 10:52:45 +0100

    Modifie page_evenement

--- eventease/vues/events/display.php
+++ eventease/vues/events/display.php
@@ -1,10 +1,26 @@
 <div class="wrapper">
     <div class ="intro_evenement">
+        <div class ="infos">
+            <h1>Nom de l'événement </h1>
+            <ul>
+                <li> <i class="maclasse fa fa-calendar-o"></i> Date <br> Heure </li>
+                <li> Lieu </li>
+        </div>
         <div class = "photo_evenement">
-            Hello, World!
+            <img src="<?php echo IMAGES.'tomorrowland.jpg'; ?>" alt="Coucou"/>
         </div>
     </div>
     <div class ="description">
-        Goodbye, World!
+        Description
+    </div>
+    <div class = "photo">
+        Plein de photos
+    </div>
+    <div class = "organisation">
+        Organisateurs et participants
+        <i class="fa fa-calendar-o"></i>
+    </div>
+    <div class = "commentaire">
+        Plein de commentaires
     </div>
 </div>
