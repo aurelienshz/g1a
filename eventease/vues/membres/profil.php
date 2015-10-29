64d97b08d3320b4621fe60e871c1cf43394648e5
lbarraud <loann_barraud@hotmail.fr>
2015-10-29 11:19:45 +0100

    Ajout image défaut

--- eventease/vues/membres/profil.php
+++ eventease/vues/membres/profil.php
@@ -2,8 +2,11 @@
 /**** VUE : PROFIL ****/
 ?>
 <div class="wrapper">
+    <div class ="photo_profil_temp">
+        <img src="<?php echo IMAGES.'fancy_cat.jpeg'; ?>"/>
+    </div>
     <h1>Nom du profil</h1>
         <p>Description</p>
-        <a href='#'>Modifier mon profil</a>
-        <h2> Futurs évènements</h2>
+
+    <h2> Futurs évènements</h2>
 </div> <!-- /wrapper -->
