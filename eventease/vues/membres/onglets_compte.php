81ed11968c7c1407d2a88c7c6df46882425bda9e
lbarraud <loann_barraud@hotmail.fr>
2015-10-30 15:31:56 +0100

    Ajout onglets dans le profil

---
+++ eventease/vues/membres/onglets_compte.php
@@ -0,0 +1,7 @@
+<div class="profil_onglets">
+  <ul class="onglets">
+    <li class="active"><a href="profil.html"> Mon compte </a></li>
+    <li><a href="#"> Mes messages </a></li>
+    <li><a href="#"> Mes évènements </a></li>
+  </ul>
+</div>
