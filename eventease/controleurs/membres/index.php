69962ba04ad353fd63d5bb7a38b046e9f3dce37f
Aurelien <aurelien.schiltz>
2015-11-10 09:38:51 +0100

    Importe font-awesome en local

--- eventease/controleurs/membres/index.php
+++ eventease/controleurs/membres/index.php
@@ -1,23 +1,23 @@
 <?php
 /* CONTROLEUR MEMBRES */
 
-    switch($_GET['action']) {
-        case 'connexion':
-            require 'controleurs/membres/connexion.php';
-            break;
-        case 'deconnexion':
-            require 'controleurs/membres/deconnexion.php';
-            break;
-        case 'inscription':
-            require 'controleurs/membres/inscription.php';
-            break;
-        case 'profil':
-            require 'controleurs/membres/profil.php';
-            break;
-        default:
-            require 'controleurs/membres/profil.php';
-            break;
-    }
+switch($_GET['action']) {
+    case 'connexion':
+        require 'controleurs/membres/connexion.php';
+        break;
+    case 'deconnexion':
+        require 'controleurs/membres/deconnexion.php';
+        break;
+    case 'inscription':
+        require 'controleurs/membres/inscription.php';
+        break;
+    case 'profil':
+        require 'controleurs/membres/profil.php';
+        break;
+    default:
+        require 'controleurs/membres/profil.php';
+        break;
+}
 
 
 // A la fin on NE DOIT PAS se retrouver en ayant rien fait;
