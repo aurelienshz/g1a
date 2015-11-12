ebe82febf157dfdc55556296d3c8b5a5ef886011
lbarraud <loann_barraud@hotmail.fr>
2015-11-12 11:10:26 +0100

    e Veuillez saisir le message de validation pour vos modifications. Les lignes

--- eventease/vues/membres/onglets_compte.php
+++ eventease/vues/membres/onglets_compte.php
@@ -1,7 +1,7 @@
 <div class="profil_onglets">
   <ul class="onglets">
-    <li class="active"><a href="profil.html"> Mon compte </a></li>
-    <li><a href="#"> Mes messages </a></li>
-    <li><a href="#"> Mes évènements </a></li>
+    <li class="active" id="left_tab"><a href="profil.html"> Mon compte </a></li>
+    <li id="center_tab"><a href="#"> Mes messages </a></li>
+    <li id="right_tab"><a href="#"> Mes évènements </a></li>
   </ul>
 </div>
--- eventease/vues/membres/profil.php
+++ eventease/vues/membres/profil.php
@@ -7,9 +7,9 @@
             <div class="photo_profil">
                 <img alt="Nom du profil" src="<?php echo IMAGES.'pirate-cat.jpg'; ?>" title="Nom du profil" height="275" width="275"/>
             </div>
-            <p>Prénom Nom</p>
             <ul class="interaction_profil">
                 <li><a href="#">Inviter à un évènement</a></li>
+                <li><a href="#">Suivre cette personne</a></li>
                 <li><a href="#">Envoyer un message privé</a></li>
             </ul>
         </div>
@@ -18,13 +18,14 @@
             <div class="description">
                 <h5>Description</h5>
             </div>
-            <h2>Détails du profil</h2>
-                <div class="evenements_futurs">
-                    <h2> Évènements futurs</h2>
-                </div>
-                <div class="evenements_passes">
-                    <h2>Évènements passés</h2>
-                </div>
+            <div class="details_profil">
+                <h2>Détails du profil</h2>
+            <div class="evenements_futurs">
+                <h2> Évènements futurs</h2>
+            </div>
+            <div class="evenements_passes">
+                <h2>Évènements passés</h2>
+            </div>
         </div>
     </div>
 </div> <!-- /wrapper -->
