29b9d12451d8dd276ff7ad86030e6de037d7b749
AudeDM <aude.demaricourt@gmail.com>
2015-11-19 10:32:04 +0100

    Ajoute block information

--- eventease/vues/events/display.php
+++ eventease/vues/events/display.php
@@ -34,11 +34,23 @@
             </div>
         </div>
     </div>
+
     <div class ="description">
         <h2>Description</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet libero vitae
 		tincidunt egestas. Duis ultricies lobortis risus sed pretium. Nulla iaculis leo sed fringilla
 		efficitur. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				<div class="details">
+					<ul>
+						<li><span>Type:</span>...</li>
+						<li><span>Prix:</span>...</li>
+						<li><span>Visibilité:</span>...</li>
+						<li><span>Type de public:</span>...</li>
+						<li><span>Langue:</span>...</li>
+						<li><span>Sponsors:</span></li>
+						<li><span>Site Web:</span>...</li>
+					</ul>
+				</div>
     </div>
     <div class = "photo">
         <table>
@@ -83,6 +95,7 @@
 		<h3> Commentaires </h3>
 		<div class="add_comment">
             <form>
+				<img src="<?php echo IMAGES.'img.jpg'; ?>" alt="Ami"/>
                 <label>Ajouter un commentaire</label>
                 <textarea id="comment" placeholder="Ajouter un commentaire"></textarea>
     			<div class="add_media">
