e7e93f256a84d779c3513bb28049d8525a1b6f35
Aurelien <aurelien.schiltz>
2015-11-23 13:24:40 +0100

    Ajoute le changement du placeholder dans l'accueil

---
+++ eventease/scripts/bigform.js
@@ -0,0 +1,24 @@
+var textInput = document.querySelector('#bigform input[type="text"]'),
+    select = document.querySelector('#bigform select'),
+    placeholder = '';
+
+select.addEventListener(
+    'change',
+    function() {
+        switch (select.selectedIndex) {
+            case 0:
+                placeholder = 'Mots-clés'
+                break;
+            case 1:
+                placeholder = 'Adresse'
+                break;
+            case 2:
+                placeholder = 'Date'
+                break;
+            default:
+                placeholder = 'Mots-clés'
+        }
+        textInput.placeholder = placeholder;
+    },
+    false
+);
