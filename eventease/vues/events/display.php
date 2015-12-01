02181bb4e14139dc19d4f9fb7f8203308acabe53
guillaume lachaud <guillaumelachaud@MacBook-Pro-de-guillaume.local>
2015-12-01 09:22:17 +0100

    Somewhere over the rainbow! (modifie la page événement)

--- eventease/vues/events/display.php
+++ eventease/vues/events/display.php
@@ -41,42 +41,8 @@
 
 The way Dinah washed her children's faces was this: first she held the poor thing down by its ear with one paw, and then with the other paw she rubbed its face all over, the wrong way, beginning at the nose: and just now, as I said, she was hard at work on the white kitten, which was lying quite still and trying to purr—no doubt feeling that it was all meant for its good.
 
-But the black kitten had been finished with earlier in the afternoon, and so, while Alice was sitting curled up in a corner of the great arm-chair, half talking to herself and half asleep, the kitten had been having a grand game of romps with the ball of worsted Alice had been trying to wind up, and had been rolling[…]” <br>
-To be, or not to be- that is the question:
-Whether 'tis nobler in the mind to suffer
-The slings and arrows of outrageous fortune
-Or to take arms against a sea of troubles,
-And by opposing end them. To die- to sleep-
-No more; and by a sleep to say we end
-The heartache, and the thousand natural shocks
-That flesh is heir to. 'Tis a consummation
-Devoutly to be wish'd. To die- to sleep.
-To sleep- perchance to dream: ay, there's the rub!
-For in that sleep of death what dreams may come
-When we have shuffled off this mortal coil,
-Must give us pause. There's the respect
-That makes calamity of so long life.
-For who would bear the whips and scorns of time,
-Th' oppressor's wrong, the proud man's contumely,
-The pangs of despis'd love, the law's delay,
-The insolence of office, and the spurns
-That patient merit of th' unworthy takes,
-When he himself might his quietus make
-With a bare bodkin? Who would these fardels bear,
-To grunt and sweat under a weary life,
-But that the dread of something after death-
-The undiscover'd country, from whose bourn
-No traveller returns- puzzles the will,
-And makes us rather bear those ills we have
-Than fly to others that we know not of?
-Thus conscience does make cowards of us all,
-And thus the native hue of resolution
-Is sicklied o'er with the pale cast of thought,
-And enterprises of great pith and moment
-With this regard their currents turn awry
-And lose the name of action.- Soft you now!
-The fair Ophelia!- Nymph, in thy orisons
-Be all my sins rememb'red.</p>
+But the black kitten had been finished with earlier in the afternoon, and so, while Alice was sitting curled up in a corner of the great arm-chair, half talking to herself and half asleep, the kitten had been having a grand game of romps with the ball of worsted Alice had been trying to wind up, and had been rolling[…]”
+    </p>
 				<div class="details">
 		<ul>
             <li class="fixed_details">Type :</li>
