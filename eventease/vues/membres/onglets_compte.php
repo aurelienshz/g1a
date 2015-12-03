e2d43b7cd3f4eb2f0a2d41a68a0a36e6e9ca1062
Aurelien <aurelien.schiltz>
2015-12-03 01:55:36 +0100

    En cours tentative profil

--- eventease/vues/membres/onglets_compte.php
+++ eventease/vues/membres/onglets_compte.php
@@ -1,7 +1,7 @@
 <div class="profil_onglets">
   <ul class="onglets">
-    <li <?php echo $contents['ongletActif']=='profil'?'class="active"':'' ?> id="left_tab"><a href="<?php echo getLink(['membres','profil']);?>">Mon compte</a></li>
-    <li <?php echo $contents['ongletActif']=='evenements'?'class="active"':'' ?> id="center_tab"><a href="<?php echo getLink(['membres','evenements']);?>">Mes évènements</a></li>
-    <li <?php echo $contents['ongletActif']=='messages'?'class="active"':'' ?> id="right_tab"><a href="<?php echo getLink(['membres','messages']);?>">Mes messages</a></li>
+    <li class="<?php echo $contents['ongletActif']=='profil'?'active ':'' ?>left_tab"><a href="<?php echo getLink(['membres','profil']);?>">Mon compte</a></li>
+    <li class="<?php echo $contents['ongletActif']=='evenements'?'active ':'' ?>center_tab"><a href="<?php echo getLink(['membres','evenements']);?>">Mes évènements</a></li>
+    <li class="<?php echo $contents['ongletActif']=='messages'?'active ':'' ?>right_tab"><a href="<?php echo getLink(['membres','messages']);?>">Mes messages</a></li>
   </ul>
 </div>
