1497664a1c18b22ccdcdb4cb1ea2fe91f1321522
lbarraud <loann_barraud@hotmail.fr>
2015-12-04 15:04:48 +0100

    ajoute requete user et photo defaut

--- eventease/modeles/membres/getUserDetails.php
+++ eventease/modeles/membres/getUserDetails.php
@@ -3,11 +3,17 @@
 function getUserDetails($id) {
     $bdd = new PDO(DSN, DBUSER, DBPASS);
 
-    $query = $bdd->prepare('SELECT * FROM membre INNER JOIN adresse ON (membre.id_adresse=adresse.id) WHERE id = :id');
+    $query = $bdd->prepare('SELECT *
+                            FROM membre RIGHT JOIN adresse
+                            ON membre.id_adresse=adresse.id
+                           UNION
+                            SELECT * FROM membre LEFT JOIN adresse
+                            ON membre.id_adresse=adresse.id
+                            WHERE membre.id = :id');
     $query->execute(['id'=>$id]);
     
     if($query->rowCount()==1) {
-        $result = $query->fetchAll();
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
         return $result[0];
     }
     else {
