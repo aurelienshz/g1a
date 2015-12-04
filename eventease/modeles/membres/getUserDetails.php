17d380473b0d09345842fcdfd7b8b1ecf9be86bd
lbarraud <loann_barraud@hotmail.fr>
2015-12-04 14:15:27 +0100

    requete sql

--- eventease/modeles/membres/getUserDetails.php
+++ eventease/modeles/membres/getUserDetails.php
@@ -3,9 +3,9 @@
 function getUserDetails($id) {
     $bdd = new PDO(DSN, DBUSER, DBPASS);
 
-    $query = $bdd->prepare('SELECT * FROM membre WHERE id = :id');
+    $query = $bdd->prepare('SELECT * FROM membre INNER JOIN adresse ON (membre.id_adresse=adresse.id) WHERE id = :id');
     $query->execute(['id'=>$id]);
-
+    
     if($query->rowCount()==1) {
         $result = $query->fetchAll();
         return $result[0];
