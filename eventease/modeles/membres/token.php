067fcd1c2ecce82edb7949b00260ee421d8fbadb
aurelienshz <aurelien.schiltz@isep.fr>
2015-12-05 21:54:37 +0100

    Token vérif email

--- eventease/modeles/membres/token.php
+++ eventease/modeles/membres/token.php
@@ -1,26 +1,38 @@
 <?php
 
-// Grains de sel :
-$salt = ['Pi9DVwpBV6',  //  NE SURTOUT PAS CHANGER  //
-         'qHVfvczw9m',  //  CES VALEURS SOUS PEINE  //
-         '7h2WSdyWIM']; //  DE TOUT CASSER.         //
 
 function generateToken($email, $username) {
-    global $salt;
+    // Grains de sel :
+    $salt = ['Pi9DVwpBV6',  //  NE SURTOUT PAS CHANGER  //
+             'qHVfvczw9m',  //  CES VALEURS SOUS PEINE  //
+             '7h2WSdyWIM']; //  DE TOUT CASSER.         //
+    // Chaîne salée :
     $salted = $salt[0].$username.$salt[1].$email.$salt[2];
-    return sha1($salted);
+    // token
+    $token = sha1($salted);
+
+    // Stockage du token en BDD :
+    $bdd = new PDO(DSN, DBUSER, DBPASS);
+    $reqToken = $bdd -> prepare('INSERT INTO `verification_membre` VALUES (:email, :token)');
+    if($reqToken -> execute(['email'=>$email, 'token'=>$token])) {
+        return $token;
+    }
+    else {
+        return False; 
+    }
+
 }
 
 function verifyToken($token) {
     $bdd = new PDO(DSN,DBUSER,DBPASS);
 
-    $reqToken = $bdd -> prepare('SELECT id FROM verification_membre WHERE token = :token');
+    $reqToken = $bdd -> prepare('SELECT email FROM verification_membre WHERE token = :token');
     if($reqToken -> execute(['token'=> $token])) {
         if($reqToken -> rowCount() == 1) {
-            $id = $reqToken -> fetchAll()[0];
+            $email = $reqToken -> fetchAll()[0];
 
-            $reqConfirm = $bdd -> prepare('UPDATE membre SET niveau = 1 WHERE id = :id');
-            $reqConfirm -> execute(['id'=>$id]);
+            $reqConfirm = $bdd -> prepare('UPDATE membre SET niveau = 1 WHERE email = :email');
+            $reqConfirm -> execute(['email'=>$email]);
             // Gérer si tout s'est bien passé ?
             return True;
         }
