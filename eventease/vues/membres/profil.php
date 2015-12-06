92db4a6fa9d9f1891f241aefc3fdd1b8f511a137
aurelienshz <aurelien.schiltz@isep.fr>
2015-12-06 19:03:30 +0100

    Proposition cosmétique profil

--- eventease/vues/membres/profil.php
+++ eventease/vues/membres/profil.php
@@ -7,24 +7,29 @@
             <div class="photo_profil">
                 <img alt="Nom du profil" src="<?php echo $contents['photo']; ?>" title="Nom du profil" height="230" width="230"/>
             </div>
+<?php if(!connected()) { ?>
             <ul id="interaction_profil">
                 <li><a class="button" href="#">Inviter à un évènement</a></li>
                 <li><a class="button" href="#">Suivre</a></li>
                 <li><a class="button" href="#">Envoyer un<br /> message privé</a></li>
             </ul>
+<?php }
+    else {  // connected()
+?>
+            <ul id="interaction_profil">
+                <li><a class="button" href="<?php echo getLink(['membres','modification_profil'])?>">Modifier mon profil</a></li>
+            </ul>
+<?php } // connected() ?>
         </div>
         <div class="col_droite">
             <h2><?php echo $contents['pseudo']; ?> <span id="statut"><?php echo $contents['statut']; ?></span></h2>
             <div id="general">
-                <h3>Informations générales :</h2>
-                <p>
-                    <ul>
-                        <li><strong>Nom : </strong><?php echo $contents['nom']; ?></li>
-                        <li><strong>Prénom : </strong><?php echo $contents['prenom']; ?></li>
-                        <li><strong>Date de naissance : </strong><?php echo $contents['ddn']; ?></li>
-                        <li><strong>Langue : </strong><?php echo $contents['langue']; ?></li>
-                    </ul>
-                </p>
+                <ul>
+                    <li><strong>Nom : </strong><?php echo $contents['nom']; ?></li>
+                    <li><strong>Prénom : </strong><?php echo $contents['prenom']; ?></li>
+                    <li><strong>Date de naissance : </strong><?php echo $contents['ddn']; ?></li>
+                    <li><strong>Langue : </strong><?php echo $contents['langue']; ?></li>
+                </ul>
             </div>
             <div class="description">
                 <h3>Description</h3>
@@ -46,6 +51,6 @@
                 </div> -->
             </div>
         </div>
-        <div id="profil-buffer"></div>
+            <div id="clearfix"></div>
     </div>
 </div>
