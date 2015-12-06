4ca72c9246dbd37e424503e87056a0670c67b810
glachaud <guillaume.lachaud78@gmail.com>
2015-12-07 00:35:11 +0100

    Améliore encore les liens à la base de données de la page événement

--- eventease/controleurs/events/display.php
+++ eventease/controleurs/events/display.php
@@ -9,15 +9,25 @@ require MODELES.'events/getEvents.php';
 
 
 // Chargement des paramètres de la page
-$title = 'Affichage d\'un évènement';
+$title = "'Affichage d\'un évènement'";
 $styles = ['events.css','form.css'];
 $scripts = ['alert.js','slideshow.js','slideshow_event.js'];
 $blocks = ['display'];
-
 $event = getEvents($_GET['id']);
+$type = EventType($_GET['id']);
+$contents['type']=$type[0];
+$site = Sponsor($_GET['id']);
+$contents['sponsor']=$site[0];
+$images = GetImages($_GET['id']);
+$contents['images']=$images[0];
+$creator=GetCreator($_GET['id']);
+$contents['creator']=$creator[0];
+$comment = GetComments($_GET['id']);
+$contents['comment']=$comment[0];
 $contents['titreEvenement'] = $event['titre'];
 $contents['tarif'] = $event['tarif'];
-$contents['type_public']=$event['type_public'];
+$contents['age_min']=$event['age_min'];
+$contents['age_max']=$event['age_max'];
 $contents['description']=$event['description'];
 $contents['site']=$event['site'];
 switch ($event['langue']){
@@ -28,9 +38,18 @@ switch ($event['langue']){
     $contents['langue']='Anglais';
     break;
 }
+switch ($event['confidentiel']){
+  case 0:
+    $contents['visibilite']='Public';
+    break;
+  case 1:
+    $contents['visibilite']='Privé';
+    break;
+}
 $contents['date'] = date('Y-m-d',strtotime($event['debut']));
+list($contents['year'], $contents['month'], $contents['day'])=explode ('-', $contents['date']);
 $contents['heure_debut'] = date('H:i:s',strtotime($event['debut']));
 $contents['heure_fin'] = date('H:i:s',strtotime($event['fin']));
 /**** Affichage de la page ****/
 //Appels des vues :
-vue($blocks, $styles, $title, $contents, $scripts);
+vue($blocks, $styles, $title, $contents, $scripts );
