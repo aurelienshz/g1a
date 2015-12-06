7ba220099cff72216d85a815186642a7d30b5d3b
ohhopi <tristan.muratore@gmail.com>
2015-12-07 00:02:06 +0100

    Ajoute des fonctions à google.php

--- eventease/modeles/functions/google.php
+++ eventease/modeles/functions/google.php
@@ -1,7 +1,5 @@
 <?php
-
-function googleCoordToAddress($lat,$long){
-	$url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$long&key=AIzaSyC02_hTBcl3SqHYTvraoftcwhPxkRSeCWA";
+function curlGoogleDecodeJson($url){
 	$curl = curl_init();
 	curl_setopt($curl, CURLOPT_URL, $url);
 	curl_setopt($curl, CURLOPT_HEADER, false);
@@ -10,6 +8,26 @@ function googleCoordToAddress($lat,$long){
 	curl_setopt($curl, CURLOPT_ENCODING, "");
 	$curlData = curl_exec($curl);
 	curl_close($curl);
-	$address = json_decode($curlData);
+	return json_decode($curlData);
+}
+
+function googleCoordToAddress($lat,$long){
+	$url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$long&key=AIzaSyC02_hTBcl3SqHYTvraoftcwhPxkRSeCWA";
+	$address = curlGoogleDecodeJson($url);
 	return $address -> results[0] -> formatted_address;
+}
+function googleAddressToCoord($address){
+	$url = "https://maps.googleapis.com/maps/api/geocode/json?address=$address&key=AIzaSyC02_hTBcl3SqHYTvraoftcwhPxkRSeCWA";
+	$coord = curlGoogleDecodeJson($url);
+	$lat = $coord -> results[0] -> geometry -> location-> lat;
+	$lng = $coord -> results[0] -> geometry -> location -> lng;
+	return array($lat,$lng);
+}
+function googleCheckAddress($address){
+	$test = googleAddressToCoord($address);
+	if(is_float($test[0]) AND is_float($test[1]) ){
+		return True;
+	}else{
+		return False;
+	}
 }
