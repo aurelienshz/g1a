60253591709095ce9b632395079b8507baac59d6
ohhopi <tristan.muratore@gmail.com>
2015-12-07 15:22:22 +0100

    Ajoute une version quasi fonctionelle de la modif de profil

--- eventease/vues/membres/modification_profil.php
+++ eventease/vues/membres/modification_profil.php
@@ -37,28 +37,28 @@
 					</div>
 					<div class="champ">
 						<label for="nom">Nom : </label>
-						<input type="text" name="nom" id="nom" value="<?php echo $contents["nom"];?>">
+						<input type="text" name="nom" id="nom" value="<?php echo htmlspecialchars($contents["nom"]);?>">
 						<?php echo isset($contents['errors']['nom'])?$contents['errors']['nom']:'' ?>
 					</div>
 					<div class="champ">
 						<label for="prenom">Prénom : </label>
-						<input type="text" name="prenom" id="prenom" value="<?php echo $contents["prenom"];?>"/>
+						<input type="text" name="prenom" id="prenom" value="<?php echo htmlspecialchars($contents["prenom"]);?>"/>
 						<?php echo isset($contents['errors']['prenom'])?$contents['errors']['prenom']:'' ?>
 					</div>
 					<div class="champ">
 						<label for="ddn">Date de naissance :</label>
-						<input type="date" name="ddn" id="date" value="<?php echo $contents["ddn"];?>"/>
+						<input type="date" name="ddn" id="date" value="<?php echo htmlspecialchars($contents["ddn"]);?>" placeholder="AAAA-MM-JJ"/>
 						<?php echo isset($contents['errors']['ddn'])?$contents['errors']['ddn']:'' ?>
 					</div>
 				</div>
 				<div class="ligne">
 					<div class="champ" style="width:20%;">
 						<label for="tel">Téléphone : </label>
-						<input type="text" name="tel" id="tel" value="<?php echo $contents["tel"];?>"/>
+						<input type="text" name="tel" id="tel" value="<?php echo htmlspecialchars($contents["tel"]);?>"/>
 					</div>
 					<div class="champ">
 						<label for="adresse">Adresse : </label>
-						<input type="text" name="adresse" id="adresse" value="<?php echo $contents["adresse"]; ?>"/>
+						<input type="text" name="adresse" id="adresse" value="<?php echo htmlspecialchars($contents["adresse"]); ?>"/>
 					</div>
 					<div class="champ">
 						<label for="langue">Langue  : </label>
@@ -67,7 +67,7 @@
 								$array = array (0 => "Français",1 => "Anglais");
 								foreach($array as $value=>$name)
 								{
-								    if($value == $contents['civilite'])
+								    if($value == $contents['langue'])
 								    {
 								         echo "<option selected='selected' value='".$value."'>".$name."</option>";
 								    }
@@ -84,14 +84,14 @@
 					<div class="champ">
 						<div class="photo_profil">
 							<label>Photo :</label>
-                			<img alt="Photo de Profil" src="<?php echo (PHOTO_PROFIL.$contents["lien_photo"]);?>" title="Photo de Profil" height="150" width="150"/> 
+                			<img alt="Photo de Profil" src="<?php echo htmlspecialchars((PHOTO_PROFIL.$contents["lien_photo"]));?>" title="Photo de Profil" height="150" width="150"/> 
 							<label for="photo"><br>Modifier ma photo de profil :</label>
 							<input type="file" id="photo" name="photo"/>
             			</div>	
 					</div>
 					<div class="champ" style="width: calc(185% /3)">
 						<label>Description :</label>
-						<textarea name="description" id="description"><?php echo $contents["description"];?></textarea>
+						<textarea name="description" id="description"><?php echo htmlspecialchars($contents["description"]);?></textarea>
 					</div>
 				</div>
 				<input type="submit" value="Modifier" />
