b47c6a289f4ccab2a51ecf4ef3ca98a6f24189b8
ohhopi <tristan.muratore@gmail.com>
2015-12-07 15:20:17 +0100

    Corrige l'encodage UTF-8 des requêtes

--- eventease/config/db.php
+++ eventease/config/db.php
@@ -9,6 +9,6 @@ AJOUTEZ-LE DANS VOTRE FICHIER .gitignore
 Merci, bisous.
 */
 
-define('DSN', 'mysql:dbname=eventease;host=127.0.0.1');
+define('DSN', 'mysql:dbname=eventease;host=127.0.0.1;charset=utf8');
 define('DBUSER', "root");
 define('DBPASS', "");
