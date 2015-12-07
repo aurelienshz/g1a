ff6436cdac5453ee3964d5d3d0bc1849c5653063
glachaud <guillaume.lachaud78@gmail.com>
2015-12-07 15:25:17 +0100

    Règle le problème

--- eventease/config/db.php
+++ eventease/config/db.php
@@ -9,6 +9,10 @@ AJOUTEZ-LE DANS VOTRE FICHIER .gitignore
 Merci, bisous.
 */
 
-define('DSN', 'mysql:dbname=eventease;host=127.0.0.1');
+define('DSN', 'mysql:dbname=eventease;host=127.0.0.1;charset=utf8');
 define('DBUSER', "root");
 define('DBPASS', "");
+
+/*define('DSN', 'mysql:dbname=eventease;host=94.23.145.193;charset=utf8');
+define('DBUSER', "eventease");
+define('DBPASS', "349bzZbbjz6Feauq");*/
