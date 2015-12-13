9f06e10b51ec03eea1ddf395010957ea8b622460
audypods <avc1.roussel@orange.fr>
2015-12-13 14:57:07 +0100

    page forum

--- eventease/controleurs/forum/index.php
+++ eventease/controleurs/forum/index.php
@@ -1,2 +1,17 @@
 <?php
-echo '<h1>Forum</h1>';
+/* CONTROLEUR D'ACTION */
+
+/**** Préparation des contenus ****/
+
+$contents = [];
+
+/**** Préparation de la vue ****/
+$title = 'Accueil - Forum';
+$styles = ['forum.css','search.css'];
+$blocks = ['forum'];
+
+
+/**** Affichage de la page ****/
+//Appel de la vue :
+vue($blocks,$styles,$title,$contents);
+?>
---
+++ eventease/vues/forum/forum.php
@@ -0,0 +1,10 @@
+<div class="wrapper createCalendar">
+  <div class="shadow">
+    <div class="titleWrapper1">
+      <img class="calendarPin" src="<?php echo IMAGES.'calendar_pin.png'; ?>">
+      <h2><i class="fa fa-comment"></i> Forum - Catégories </h2>
+      <img class="calendarPin calendarPin2" src="<?php echo IMAGES.'calendar_pin.png'; ?>">
+    </div>
+  </div>
+</div>
+
