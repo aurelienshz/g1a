9353a0b0de2ab9d6702e1b1d2a7230454078b224
audypods <avc1.roussel@orange.fr>
2015-12-13 17:58:05 +0100

    amélioration css page du forum

--- eventease/vues/forum/forum.php
+++ eventease/vues/forum/forum.php
@@ -1,10 +1,57 @@
 <div class="wrapper createCalendar">
   <div class="shadow">
     <div class="titleWrapper1">
-      <img class="calendarPin" src="<?php echo IMAGES.'calendar_pin.png'; ?>">
+      <img class="calendarPin" src="<?php echo IMAGES.'calendar_pin_green.png'; ?>">
       <h2><i class="fa fa-comment"></i> Forum - Catégories </h2>
-      <img class="calendarPin calendarPin2" src="<?php echo IMAGES.'calendar_pin.png'; ?>">
+      <img class="calendarPin calendarPin2" src="<?php echo IMAGES.'calendar_pin_green.png'; ?>">
+    </div>
+    <div class="categorie_forum">
+      <h2>Aide</h2>
+      <h3>Pour régler les problèmes que vous rencontrez avec le site</h3>
+      <h2>Suggestions</h2>
+      <h3>Si vous avez des suggestions pour améliorer le site</h3>
+      <h2>Idées d'évènements</h2>
+      <h3>Si vous ne savez pas quel évènement organiser</h3>
+      <h2>Liens utiles</h2>
+      <h3>Les sites pour vous aider dans la création d'évènement</h3>
+      <h2>Bugs</h2>
+      <h3>Pour nous signaler les bugs du site</h3>
     </div>
   </div>
 </div>
 
+
+
+
+<!--<header class="tete">
+        <div class="cellule">Forums</div>
+        <div class="cellule">Sujets</div>
+        <div class="cellule">Messages</div>
+      </header>
+     
+      <div class="corp">
+        <div class="ligne">
+          <div class="cellule"><h2>Aide</h2>Pour régler les problèmes que vous rencontrez avec le site</div>
+          <div class="cellule">2</div>
+          <div class="cellule">3</div>
+        </div>
+        <div class="ligne">
+          <div class="cellule"><h2>Suggestions</h2>Si vous avez des suggestions pour améliorer le site</div>
+          <div class="cellule">2</div>
+          <div class="cellule">3</div>
+        </div>
+        <div class="ligne">
+          <div class="cellule"><h2>Idées d'événement</h2>Si vous ne savez pas quel évènement organiser</div>
+          <div class="cellule">2</div>
+          <div class="cellule">3</div>
+        </div>
+        <div class="ligne">
+          <div class="cellule"><h2>Liens utiles</h2>Les sites pour vous aider dans la création d'évènement</div>
+          <div class="cellule">2</div>
+          <div class="cellule">3</div>
+        </div>
+        <div class="ligne">
+          <div class="cellule"><h2>Bugs</h2>Pour nous signaler les bugs du site</div>
+          <div class="cellule">2</div>
+          <div class="cellule">3</div>
+        </div>
