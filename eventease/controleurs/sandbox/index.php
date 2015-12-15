e8c66bea962038c9dfcfd2bc7889a5be7044bc28
Aurelien <aurelien.schiltz>
2015-12-15 09:25:47 +0100

    ~~

--- eventease/controleurs/sandbox/index.php
+++ eventease/controleurs/sandbox/index.php
@@ -6,7 +6,7 @@ echo '<h1>SANDBOX ~ EVENTEASE</h1>';
 require MODELES.'events/searchEvents.php';
 
 echo '<pre>';
-$res = searchEvents('a');
+$res = searchEvents('concert');
 
 echo '<h2>Résultats retournés :</h2>';
 var_dump($res);
