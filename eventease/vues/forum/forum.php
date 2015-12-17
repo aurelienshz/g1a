088c3846598eacbb68116d6bad1b8c08f69fabd3
audypods <avc1.roussel@orange.fr>
2015-12-17 09:29:17 +0100

    page sujet du forum

--- eventease/vues/forum/forum.php
+++ eventease/vues/forum/forum.php
@@ -3,10 +3,10 @@
   <div class="shadow">
     <div class="titleWrapper1">
       <img class="calendarPin" src="<?php echo IMAGES.'calendar_pin_green.png'; ?>">
-      <h2><i class="fa fa-comment"></i> Forum - Catégories </h2>
+      <h2><i class="fa fa-comments"></i>  Forum</h2>
       <img class="calendarPin calendarPin2" src="<?php echo IMAGES.'calendar_pin_green.png'; ?>">
     </div>
-    <div class="categorie_forum">
+    <!--<div class="categorie_forum">
       <!--<?php foreach ($contents as $section) { 
         echo $section . '<br/>'; } ?>-->
     <div class="tableau">
@@ -15,14 +15,25 @@
       </div>
       <div class="content">
         <div class="question">
+          <a href="<?php echo getLink(['forum','sujet'])?>">
           <div class="enonce">Je n'arrive pas à me connecter</div>
           <div class="reponses">48 réponses</div>
           <div class="auteur">par <strong>Audodo</strong></div>
+          </a>
         </div>
         <div class="question">
+          <a href="<?php echo getLink(['forum','sujet'])?>">
           <div class="enonce">Comment changer sa photo de profil?</div>
           <div class="reponses">2 réponses</div>
           <div class="auteur">par <strong>audypods</strong></div>
+          </a>
+        </div>
+        <div class="question">
+          <a href="<?php echo getLink(['forum','sujet'])?>">
+          <div class="enonce">Peut-on modifier son profil?</div>
+          <div class="reponses">61 réponses</div>
+          <div class="auteur">par <strong>ellie-smiley</strong></div>
+          </a>
         </div>
       </div>
     </div>
@@ -32,14 +43,18 @@
       </div>
       <div class="content">
         <div class="question">
+          <a href="<?php echo getLink(['forum','sujet'])?>">
           <div class="enonce">Des idées de sortie pour les enfants?</div>
           <div class="reponses">32 réponses</div>
           <div class="auteur">par <strong>KevinDu38</strong></div>
+          </a>
         </div>
         <div class="question">
-          <div class="enonce">Meilleur endroit pour un pique-nique à Paris</div>
+          <a href="<?php echo getLink(['forum','sujet'])?>">
+          <div class="enonce">Meilleur endroit pour un pique-nique à Paris?</div>
           <div class="reponses">6 réponses</div>
           <div class="auteur">par <strong>Apolito</strong></div>
+          </a>
         </div>
       </div>
     </div>
