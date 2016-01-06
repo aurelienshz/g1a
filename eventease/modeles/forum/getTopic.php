117c6fbc45ce227e89eba500d0bb4c720d5d467c
audypods <avc1.roussel@orange.fr>
2016-01-06 10:08:46 +0100

    affiche les topics par date decroissante

--- eventease/modeles/forum/getTopic.php
+++ eventease/modeles/forum/getTopic.php
@@ -1,7 +1,7 @@
 <?php
 function getTopic($id_section) {
   $bdd = new PDO(DSN, DBUSER, DBPASS);
-  $query = $bdd->prepare('SELECT topic.titre, topic.id_auteur, membre.pseudo FROM topic, membre WHERE id_section = :id_section AND topic.id_auteur=membre.id');
+  $query = $bdd->prepare('SELECT topic.titre, topic.id_auteur, membre.pseudo FROM topic, membre WHERE id_section = :id_section AND topic.id_auteur=membre.id ORDER BY date_creation DESC');
   $query-> execute(['id_section'=>$id_section]);
   $topic = $query->fetchAll();
 
