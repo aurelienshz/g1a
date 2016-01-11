ebff9fff356b4f9ebfbc36ed9642d635e4cee309
ohhopi <tristan.muratore@gmail.com>
2016-01-11 17:11:05 +0100

    Commence à intégrer la modif d'event

--- eventease/modeles/events/checkOrganiser.php
+++ eventease/modeles/events/checkOrganiser.php
@@ -7,18 +7,24 @@ function checkOrganiser($userId, $eventId){
     $queryMod = $bdd->prepare("SELECT membre.id FROM membre, modere WHERE membre.id = modere.id_organisateur AND modere.id_evenement = $eventId;");
 	
 	$queryCreator -> execute();
-	$queryMod -> execute();
 	$creatorId = $queryCreator -> fetchAll();
+	$queryMod -> execute();
 	$moderatorId = $queryMod -> fetchAll();
 	// Construction Array de ID autorisés.
-	$id = [$creatorId[0][0]];
-	foreach ($moderatorId as $key => $value) {
-		array_push($id,$moderatorId[$key][0]);
-	}
-	// Test des ID Autorisés
-	if (in_array($userId, $id)){
-		return True;
+	if (!empty($creatorId)){
+
+		$id = [$creatorId[0][0]];
+		foreach ($moderatorId as $key => $value) {
+			array_push($id,$moderatorId[$key][0]);
+		}
+		// Test des ID Autorisés
+		if (in_array($userId, $id)){
+			return True;
+		}else{
+			return False;
+		}   
+		 
 	}else{
 		return False;
-	}    
+	}
 }
