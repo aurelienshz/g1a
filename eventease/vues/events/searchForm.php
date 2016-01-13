7f3b16e198788644288c10f6b83538b8e54c800d
lbarraud <loann_barraud@hotmail.fr>
2016-01-13 17:41:24 +0100

    événement --> évènement
    question de cohérence dans le site

--- eventease/vues/events/searchForm.php
+++ eventease/vues/events/searchForm.php
@@ -1,7 +1,7 @@
 <div class="wrapper prettyform shadow">
 	<div class="title-wrapper">
 		<img class="calendarPin" src="<?php echo IMAGES.'calendar_pin_blue.png'; ?>">
-		<h2><i class="fa fa-search"></i> Chercher un événement</h2>
+		<h2><i class="fa fa-search"></i> Chercher un évènement</h2>
 		<img class="calendarPin calendarPin2" src="<?php echo IMAGES.'calendar_pin_blue.png'; ?>">
 	</div>
 	<form method="post" action="<?php echo getLink(['events','search']); ?>">
@@ -25,7 +25,7 @@
 		</div>
 	</form>
 	<div class="tabs">
-		<div class="tab" id="tab-events">Chercher un événement</div>
+		<div class="tab" id="tab-events">Chercher un évènement</div>
 
 		<div class="tab" id="tab-users"><a href="#"><p>Chercher un utilisateur</p></a></div>
 	</div>
