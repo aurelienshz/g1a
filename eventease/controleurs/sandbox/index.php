5dcd58116284026fd738c7fcd90371aec3b36b9b
aurelienshz <aurelien.schiltz@free.fr>
2016-01-14 10:40:28 +0100

    user -> member

--- eventease/controleurs/sandbox/index.php
+++ eventease/controleurs/sandbox/index.php
@@ -3,13 +3,11 @@ echo '<h1>SANDBOX ~ EVENTEASE</h1>';
 
 // -------------------------------------------------- //
 
-require MODELES.'accueil/getCatchphrases.php';
-require MODELES.'backoffice/setCatchphrases.php';
-require MODELES.'faq/getPosts.php';
+require MODELES.'events/getMemberEvents.php';
 
-$ress = getPosts();
+$res = getMemberEvents(1);
 
 echo '<h2>Résultats retournés :</h2>';
 echo '<pre>';
-var_dump($ress);
+var_dump($res);
 echo '</pre>';
