4f1680b6b07153f3278c1ee89427cb9de91bd299
audypods <avc1.roussel@orange.fr>
2016-01-15 15:09:56 +0100

    debut php formulaire d'aide (ca marche pas)

--- eventease/controleurs/aide/formulaire_contact.php
+++ eventease/controleurs/aide/formulaire_contact.php
@@ -4,6 +4,9 @@ require MODELES. 'faq/getUserInfo.php';
 /**** Préparation de la vue ****/
 $contents=getUserInfo($_SESSION['id']);
 
+
+
+
 $title = 'Nous contacter';
 $styles = ['forum.css','search.css','form.css'];
 $blocks=['formulaire_contact'];
@@ -11,6 +14,23 @@ $blocks=['formulaire_contact'];
 /**** Affichage de la page ****/
 //Appel de la vue :
 
-vue($blocks,$styles, $title, $contents);
+if ($_POST){
+	$contents['nom']=$_POST['nom'];
+	$contents['message']=$_POST['message'];
+	$contents['email']=$_POST['email'];
+	$passage_ligne="\r\n";
 
+	$messageTotal= "de :" . $contents['nom'] . $passage_ligne . "email :" . $contents['email'] . $passage_ligne . "Message :" . $contents['message'];
+	var_dump($messageTotal);
+	if (mail('avc1.rousselpub@gmail.com', 'Demande d\'aide', $messageTotal)){
+		echo "bou";
+	}
+	vue($blocks,$styles, $title, $contents);
+	// header('Location: '.getLink(['accueil']));
+	// exit();
+}
+
+else {
+	vue($blocks,$styles, $title, $contents);
+}
 ?>
