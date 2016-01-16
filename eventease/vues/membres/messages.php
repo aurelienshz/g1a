9a6202ff4a37c331f5d45326ab7d6203cae0e05b
lbarraud <loann_barraud@hotmail.fr>
2016-01-16 16:45:12 +0100

    messagerie still in progress

--- eventease/vues/membres/messages.php
+++ eventease/vues/membres/messages.php
@@ -1,18 +1,24 @@
 <div class="wrapper" id="listeMessages">
-    <h2>Mes messages privés</h2>
     <table>
+        <?php
+        if(!$contents['messages']) {
+            echo "<h1> Boîte de messagerie vide</h1>";
+        }
+        else {
+            echo '
+        <h2>Mes messages privés</h2>
         <tr>
             <th class="from">Emetteur</th>
             <th class="subject">Message</th>
             <th class="date">Envoyé le</th>
-        </tr>
-        <?php
+        </tr>';
         foreach($contents['messages'] as $i => $message) {
             echo '<tr'.($i%2==0?' class="bg"':'').'>'."\n";
-            echo '    <td><a href="#">'.getUserName($message['id_auteur'])[0]["pseudo"].'</a></td>'."\n";
-            echo '    <td><a href="#">'.$message['contenu'].'</a></td>'."\n";
+            echo '    <td><a href="'.getLink(["membres","profil",''.$message['id_auteur'].'']).'">'.getUserName($message['id_auteur'])[0]["pseudo"].'</a></td>'."\n";
+            echo '    <td>'.$message['contenu'].'</td>'."\n";
             echo '    <td>'.$message['date_publication'].'</td>'."\n";
             echo '</tr>'."\n";
+        }
         }
         ?>
     </table>
