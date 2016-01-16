bfb056b2c7c37e9024a305642c26b8c3cc74c5fe
lbarraud <loann_barraud@hotmail.fr>
2016-01-17 00:17:12 +0100

    à demain !
    P.S. : il faut la version de la BDD de ohhopi pour que la messagerie
    marche mais je connais pas le protocole pour faire le changement

--- eventease/modeles/membres/sendPrivateMessage.php
+++ eventease/modeles/membres/sendPrivateMessage.php
@@ -4,7 +4,7 @@ function sendPrivateMessage($message_prive, $id_destinataire, $id_auteur) {
 	$query = $bdd->prepare('INSERT INTO
 		message_prive (contenu, id_auteur, date_publication, id_destinataire)
     	VALUES (:contenu, :id_auteur, NOW(), :id_destinataire);');
-    $execute = [':contenu' => $message_prive, ':id_auteur'=>$id_auteur, ':id_destinataire' => $id_destinataire];
+    $execute = ['contenu' => $message_prive, 'id_auteur'=>$id_auteur, 'id_destinataire' => $id_destinataire];
 
     if($query -> execute($execute)){
       return True;
