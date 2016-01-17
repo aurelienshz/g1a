9446a2408f2131eee2a1b0eb745b4327d63275d1
glachaud <guillaume.lachaud78@gmail.com>
2016-01-17 19:26:16 +0100

    Ajax, encore

--- eventease/modeles/events/setParticipe.php
+++ eventease/modeles/events/setParticipe.php
@@ -2,9 +2,6 @@
 $etat = $_REQUEST["p"];
 $q = $_REQUEST["q"];
 $r = $_REQUEST["r"];
-define('DSN', 'mysql:dbname=eventease;host=127.0.0.1;charset=utf8');
-define('DBUSER', "root");
-define('DBPASS', "");
 $bdd = new PDO(DSN,DBUSER,DBPASS);
 if ($etat==1){
     $query =$bdd->prepare('INSERT INTO invitation (etat, id_evenement, id_destinataire) VALUES(1,?,?)');
