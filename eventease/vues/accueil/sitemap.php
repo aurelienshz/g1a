c56d1782ec6499f9b15b17c8db32acfcba0f5638
aurelienshz <aurelien.schiltz@free.fr>
2016-01-17 23:37:27 +0100

    sitemap

--- eventease/vues/accueil/sitemap.php
+++ eventease/vues/accueil/sitemap.php
@@ -1,12 +1,21 @@
 <div class="wrapper-jumbotext">
     <h1>Plan du site</h1>
     <ul>
-        <li>Catégorie</li>
+        <li><a href="<?php echo getLink(['accueil']); ?>">Accueil</a></li>
+        <li>Évènements</li>
         <ul>
-            <li>Page</li>
-            <li>Page</li>
+            <li><a href="<?php echo getLink(['events','create']); ?>">Créer</a></li>
+            <li><a href="<?php echo getLink(['events','search']); ?>">Chercher</a></li>
         </ul>
-        <li>Catégorie</li>
-        <li>Catégorie</li>
+        <li>Membres</li>
+        <ul>
+            <li><a href="<?php echo getLink(['membres','connexion']); ?>">Connexion</a></li>
+            <li><a href="<?php echo getLink(['membres','inscription']); ?>">Inscription</a></li>
+        </ul>
+        <li><a href="<?php echo getLink(['forum']); ?>">Forum</a></li>
+        <li><a href="<?php echo getLink(['accueil','about']); ?>">A propos</a></li>
+        <li><a href="<?php echo getLink(['accueil','cgu']); ?>">Conditions générales d'utilisation</a></li>
+        <li><a href="<?php echo getLink(['accueil','legal']); ?>">Mentions légales</a></li>
+
     </ul>
 </div>
